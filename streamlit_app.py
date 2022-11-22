557f3b2c0fea761c9fdb7fb26dd54bb956d21c57
TwoOneRed <102686099+TwoOneRed@users.noreply.github.com>
2022-11-22 15:18:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 
-#data = pd.read_csv('data.csv')
+data = pd.read_csv('data.csv')
 
 #st.text(data['SmokerStatus'].unique())
 
@@ -13,9 +13,6 @@ import streamlit as st
 
 import pydeck as pdk
 
-chart_data = pd.DataFrame(
-   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-   columns=['lat', 'lon'])
 
 st.pydeck_chart(pdk.Deck(
     map_style=None,
@@ -28,7 +25,7 @@ st.pydeck_chart(pdk.Deck(
     layers=[
         pdk.Layer(
            'HexagonLayer',
-           data=chart_data,
+           data=data,
            get_position='[lon, lat]',
            radius=200,
            elevation_scale=4,
@@ -38,7 +35,7 @@ st.pydeck_chart(pdk.Deck(
         ),
         pdk.Layer(
             'ScatterplotLayer',
-            data=chart_data,
+            data=data,
             get_position='[lon, lat]',
             get_color='[200, 30, 0, 160]',
             get_radius=200,
