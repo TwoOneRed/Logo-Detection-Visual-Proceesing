5fd453081dca8041b7bf37377e157fdbf7cb7875
TwoOneRed <102686099+TwoOneRed@users.noreply.github.com>
2022-11-22 15:21:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,38 +7,8 @@ import streamlit as st
 
 data = pd.read_csv('data.csv')
 
-#st.text(data['SmokerStatus'].unique())
+st.text(data['SmokerStatus'].unique())
 
-#st.map(data)
-
-import pydeck as pdk
+st.map(data)
 
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
