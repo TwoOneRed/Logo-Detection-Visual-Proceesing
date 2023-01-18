7ba8232f6fa6699f2b9a2324ead614248b7a652a
TAN SIN ZHUNG <tansinzhung@gmail.com>
2023-01-18 16:16:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 #IMPORT LIBRARY
 from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
-import collections
+import cv2
 import numpy as np
 import math
 import collections
 
-st.text('tansinzhung')
+st.Title('Logo Retrieval and Recognition System')
+upload_file = st.file_uploader('Please upload an Image file', accept=["jpg", "jpeg", "png", "gif", "jfif"])
 
+if upload_file is not None:
+   st.success("Image file uploaded")
 
-
-                      
+   # Read the file to a dataframe using cv2
+   img = cv2.imread('upload_file', cv2.IMREAD_GRAYSCALE)
+   st.image(img, caption='Uploaded Image', use_column_width=True)                
