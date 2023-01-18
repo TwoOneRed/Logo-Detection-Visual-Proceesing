7d6e0465d9f74bbe1956b546b83e22e561fb8134
TwoOneRed <leong_2001@hotmail.com>
2023-01-19 00:09:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,23 +72,14 @@ if upload_file is not None:
     threshold_value = st.slider("Select Threshold Value", 0, 255, 150)
 
     # perform gaussianBlur
-    img_blur = cv2.GaussianBlur(processimage, (5, 5), 0)
+    img_blur = cv2.GaussianBlur(normalized_image, (5, 5), 0)
 
     # convert colorspace
     grayImage = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
 
-    # perform gaussianBlur
-    img_blur1 = cv2.GaussianBlur(normalized_image, (5, 5), 0)
-
-    # convert colorspace
-    grayImage1 = cv2.cvtColor(img_blur1, cv2.COLOR_BGR2GRAY)
-
     # thresholding
     ret, thres = cv2.threshold(grayImage, threshold_value, 255, cv2.THRESH_BINARY)
     st.image(thres, caption='Processed Image', use_column_width=True)
-
-    ret, thres = cv2.threshold(grayImage1, threshold_value, 255, cv2.THRESH_BINARY)
-    st.image(thres, caption='Normalizeds Image', use_column_width=True)
 
     edges = cv2.Canny(thres, 50, 150)
     st.image(edges, caption='Edged Image', use_column_width=True)
