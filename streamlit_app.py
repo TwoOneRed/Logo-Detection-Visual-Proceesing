d3c1eda4760e13507aff240771ae506f8a57db8f
TAN SIN ZHUNG <tansinzhung@gmail.com>
2023-01-18 16:20:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,12 @@ import math
 import collections
 
 st.Title('Logo Retrieval and Recognition System')
-upload_file = st.file_uploader('Please upload an Image file', accept=["jpg", "jpeg", "png", "gif", "jfif"])
+upload_file = st.file_uploader('Please upload an Image file', type=["jpg", "jpeg", "png", "gif", "jfif"])
 
 if upload_file is not None:
    st.success("Image file uploaded")
 
    # Read the file to a dataframe using cv2
-   img = cv2.imread('upload_file', cv2.IMREAD_GRAYSCALE)
+   img = np.fromfile(upload_file, np.uint8)
+   img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
    st.image(img, caption='Uploaded Image', use_column_width=True)                
