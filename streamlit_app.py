f918a69ce60784a4b3ae5b0fcc4c3b7994656378
TwoOneRed <leong_2001@hotmail.com>
2023-01-20 00:37:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,9 +93,9 @@ if upload_file is not None:
 
         st.text(best_matches[0][1])
 
-        curr = imagedataset[imagedataset['filename'] == best_matches[0][0]]['image']
+        curr = np.squeeze(imagedataset[imagedataset['filename'] == best_matches[0][0]]['image'])
         st.text(curr)
-        st.image(curr, caption='Cropped Image', use_column_width=True)
+        st.image(Image.fromarray(curr), caption='Cropped Image', use_column_width=True)
 
 
 
